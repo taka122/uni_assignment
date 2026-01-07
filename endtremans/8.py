1107e8baa6ce0e4dcbede19215e94a4e90326442
taka122 <manjyuu.1418.1221@gmail.com>
2026-01-07 18:12:30 +0900

    期末の問題文

--- endtremans/8.py
+++ endtremans/8.py
@@ -1,14 +1,14 @@
 class Student:
 	def __init__(self, name, id):
-		self. name = name
-		self. id = id
+		self.name = name
+		self.id = id
 	def change_name(self, new_name):
-		self. name = new_name
+		self.name = new_name
 name = input("文字列：")
 id = int(input ("id:"))
-taro = Student (name, id)
-print (f" {taro.name} {taro.id}")
+s = Student (name, id)
+print (f" {s.name} {s.id}")
 
 new_name = input("新しい名前：")
-taro.change_name(new_name)
-print (f" {taro.name} {taro.id}")
+s.change_name(new_name)
+print (f"{s.name} {s.id}")
